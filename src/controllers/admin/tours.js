24099481a57280cacc8edeefcec144df55259516
ducnt <nguyentheduc15196@gmail.com>
2025-05-08 23:50:53 +0700

    add tour detail page

--- src/controllers/admin/tours.js
+++ src/controllers/admin/tours.js
@@ -1,5 +1,5 @@
 const moment = require("moment");
-const { Tour, Booking } = require("../../../models");
+const { Tour, Booking, Journey, User } = require("../../../models");
 const { buildTourFilter } = require("../../services/tour.service");
 
 let getTours = async (req, res) => {
@@ -10,13 +10,6 @@ let getTours = async (req, res) => {
     "1": "Quốc tế",
     "2": "Nội địa",
     "3": "Tự thiết kế",
-  };
-  const bookingStatusMap = {
-    "1": "Chờ xác nhận",
-    "2": "Đã xác nhận",
-    "3": "Đã hủy",
-    "4": "Đã hoàn thành",
-    "5": "Thất bại",
   };
 
   try {
@@ -52,6 +45,79 @@ let getTours = async (req, res) => {
   }
 };
 
+let getTourDetails = async (req, res) => {
+  const tourId = req.params.id;
+  const bookingLimit = 20;
+  const bookingPage = parseInt(req.query.bookingPage) || 1;
+  const bookingOffset = (bookingPage - 1) * bookingLimit;
+
+  try {
+    const tour = await Tour.findOne({
+      where: { id: tourId, deletedAt: null },
+      include: [{
+        model: Journey,
+        as: "journeys",
+        order: [["createdAt", "DESC"]],
+      }],
+    });
+
+    if (!tour)  return res.status(404).send("Không tìm thấy tour");
+
+    const {
+      count: bookingCount, rows: bookingRows
+    } = await Booking.findAndCountAll({
+      where: { tourId: tour.id },
+      include: [
+        {
+          model: User,
+          as: "users",
+          attributes: ["id", "name"],
+        },
+      ],
+      limit: bookingLimit,
+      offset: bookingOffset,
+      order: [["bookingDate", "DESC"]],
+    });
+
+    const tourTypeMap = {
+      "1": "Quốc tế",
+      "2": "Nội địa",
+      "3": "Tự thiết kế",
+    };
+
+    const bookingStatusMap = {
+      "1": "Chờ xác nhận",
+      "2": "Đã xác nhận",
+      "3": "Đã hủy",
+      "4": "Đã hoàn thành",
+      "5": "Thất bại",
+    };
+
+    const tourData = tour.toJSON();
+    const bookings = bookingRows.map(booking => ({
+      ...booking.toJSON(),
+      textStatus: bookingStatusMap[booking.status],
+    }));
+
+    const totalBookingPages = Math.ceil(bookingCount / bookingLimit);
+
+    res.render("admin/tours/show", {
+      tour: {
+        ...tourData,
+        textType: tourTypeMap[tourData.type],
+        bookings,
+      },
+      currentPage: bookingPage,
+      totalPages: totalBookingPages,
+      limit: bookingLimit,
+      moment
+    })
+  } catch (error) {
+    res.status(500).send("Internet Server Error");
+  }
+};
+
 module.exports = {
   getTours,
+  getTourDetails,
 };
